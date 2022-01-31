879fecc8a48d62c5655ec43feef699c28174fb5b
Eliab Lopez <eliablopez@hotmail.com>
2022-01-31 09:21:02 -0500

    Add webpack and linter configuration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import '../styles/global.scss';
+
 const App = () => {
-  return <div>Aplicación</div>;
+  return <div>Aplicación con webpack y react</div>;
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-undef */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,6 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  mode: 'development',
   resolve: {
     extensions: ['.js', '.jsx'],
   },
@@ -23,6 +26,10 @@ module.exports = {
         test: /\.html$/,
         use: [{ loader: 'html-loader' }],
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
   },
   plugins: [
@@ -30,5 +37,15 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html',
     }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
   ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+  },
 };
