023091d08289ecae046a583ed7020728ba395b93
Eliab Lopez <eliablopez@hotmail.com>
2022-02-03 18:13:38 -0500

    Add custom hook getProducts

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/ProductItem.scss';
+import PropTypes from 'prop-types';
 
-const ProductItem = () => {
+import addToCart from '@icons/bt_add_to_cart.svg';
+
+const ProductItem = ({ product }) => {
+  const { images, title, price } = product;
+  const [cart, setCart] = useState([]);
+
+  const handleClick = () => {
+    setCart([]);
+  };
+
   return (
     <div className='ProductItem'>
-      <img
-        src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-        alt=''
-      />
+      <img src={images} alt={title} />
       <div className='product-info'>
         <div>
-          <p>$120,00</p>
-          <p>Bike</p>
+          <p>${price}</p>
+          <p>{title}</p>
         </div>
-        <figure>
-          <img src='./icons/bt_add_to_cart.svg' alt='' />
+        <figure style={{ cursor: 'pointer' }} onClick={handleClick}>
+          <img src={addToCart} alt='Add to cart' />
         </figure>
       </div>
     </div>
   );
+};
+
+ProductItem.propTypes = {
+  product: PropTypes.object.isRequired,
+  images: PropTypes.string,
+  title: PropTypes.string,
+  price: PropTypes.number,
 };
 
 export default ProductItem;
